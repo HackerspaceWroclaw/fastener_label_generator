e5a13934820bf472039e6999cc085b56909e5291
małpa <bpmalpa@gmail.com>
2024-12-04 22:14:28 +0100

    Trying fonts and layouts.

--- fastener_label_generator.py
+++ fastener_label_generator.py
@@ -4,7 +4,6 @@ label_writer = LabelWriter(
     "item_template.html", default_stylesheets=("style.css",)
 )
 records = [
-    dict(thread="M3", length="30", screw_img="Fasteners_ISO7045.svg", bit_img="PH.png", bit_name = "PH2")
+    dict(thread="M56", length="986", screw_img="ISO7380_A.svg", drive_img="H.svg", drive_name_and_size = "PH2", id="202131231", material="Stal nierdzewna A2", coating="Czerniona", strength_class="Klasa 12.8", norms="ISO7810")
 ]
-
 label_writer.write_labels(records, target="labels.pdf", base_url=".")
